c52197e8e04dc0e4948d7ec150e6fe9738d03cc3
kimehhk <kimehhk@gmail.com>
2020-09-29 19:21:36 +0900

    first commit

--- mainapp/views.py
+++ mainapp/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def eventIntro(request):
+    return render(request, 'eventIntro.html')
+
+def event1(request):
+    return render(request, 'event1.html')
+
+def event2(request):
+    return render(request, 'event2.html')
