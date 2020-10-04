31c8759b41628e2c1eb4c5b3cb1c594264d8cad1
tyan <taeyeon0107@naver.com>
2020-10-04 15:53:24 +0900

    eventapp/urls.py event 철자 수정

--- eventapp/urls.py
+++ eventapp/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.event1, name="evnet1"),
-    path('', views.event2, name="evnet2"),
+    path('', views.event1, name="event1"),
+    path('', views.event2, name="event2"),
     path('index/', views.index,name='index'),
     path('create/', views.post_create,name='create'),
     path('event_post/<int:post_id>/', views.post_detail,name='detail'),
