ed2e3bb34db52788d7550588a8eaee3029d11bee
kimehhk <kimehhk@gmail.com>
2020-10-04 21:43:58 +0900

    url changed

--- eventapp/urls.py
+++ eventapp/urls.py
@@ -2,11 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.event1, name="event1"),
-    path('', views.event2, name="event2"),
     path('index/', views.index,name='index'),
     path('create/', views.post_create,name='create'),
     path('event_post/<int:post_id>/', views.post_detail,name='detail'),
     path('update/<int:post_id>/', views.post_update,name='update'),
     path('delete/<int:post_id>/',views.post_delete,name='delete'),
-    ]
+]
--- myvenv/Scripts/django-admin.py
+++ myvenv/Scripts/django-admin.py
@@ -1,4 +1,4 @@
-#!c:\users\user\desktop\부엉이\likelion8_hackathon\myvenv\scripts\python.exe
+#!c:\users\82103\desktop\likelion\hackathon\likelion8_hackathon\myvenv\scripts\python.exe
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
