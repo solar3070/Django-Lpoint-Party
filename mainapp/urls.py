6a69e073b18585b64de31336ae211c23e6a24f91
sion <aabbcc030421@gamil.com>
2020-10-25 14:11:04 +0900

    change home

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index"),
+    path('', views.home, name="home"),
     path('eventIntro/', views.eventIntro, name="eventIntro"),
     path('event1/', views.event1, name="event1"),
     path('event2/', views.event2, name="event2"),
